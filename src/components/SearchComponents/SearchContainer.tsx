5912c1a43b0970b633210813ed71b0f4fd23c4d5
Somxhai <ChanakanThongrong@gmail.com>
2022-09-25 17:02:42 +0700

    change id from facultySearch to searchContainer

--- src/components/SearchComponents/SearchContainer.tsx
+++ src/components/SearchComponents/SearchContainer.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../../styles/scrollwheel.css";
+
 import SearchResults from "./SearchResults";
 import SearchBar from "./SearchBar";
 
@@ -12,7 +12,7 @@ const SearchContainer = () => {
     <div className="px-5 w-screen h-screen absolute">
       <div
         className="hidden p-3 max-w-2xl min-w-fit fixed z-10 bg-white shadow-xl left-1/2 -translate-x-1/2 translate-y-5 rounded-lg"
-        id="FacultySearch"
+        id="searchContainer"
       >
         <SearchBar returnSearch={getSearch} />
         <SearchResults receiveSearch={searchResult} />
