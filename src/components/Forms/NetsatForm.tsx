9ec48ad743965db33ebccf30b79fe52a2a9260a3
Somxhai <ChanakanThongrong@gmail.com>
2022-09-25 16:58:16 +0700

    refactor & add Capabilities form

--- src/components/Forms/NetsatForm.tsx
+++ src/components/Forms/NetsatForm.tsx
@@ -1,33 +1,73 @@
-import SubjectInput from "../SubjectInput";
-
+import { useContext, useMemo } from "react";
+import { DataContext } from "../../features/DataContext";
+import { containerHandler } from "../../features/EssentialFeatures";
+import { hasCapability } from "../../features/GetAPI";
+import { OpenFormButton } from "../Template/Buttons";
+import { SubjectInput } from "../SubjectInput";
 
 const NetsatForm = () => {
-    return (<form
-        id="input-form"
-        className="text-sm pt-3 pb-9 m-auto text-center"
-      >
-        <p className="text-xl text-text_primary mt-5">ความฉลาดรู้ทั่วไป</p>
-        <SubjectInput title="ภาษาไทย" id="thai" />
-        <SubjectInput title="ภาษาอังกฤษ" id="eng" />
-        <SubjectInput title="คณิตศาสตร์" id="math" />
-        <p className="text-xl text-text_primary mt-5">ความฉลาดรู้เฉพาะ</p>
-        <SubjectInput title="วิทยาศาสตร์และเทคโนโลยี" id="sci" />
-        <SubjectInput title="เคมี" id="chem" />
-        <SubjectInput title="ชีววิทยา" id="bio" />
-        <SubjectInput title="ฟิสิกส์" id="phy" />
-      </form>)
-}
+  return (
+    <form id="input-form" className="text-sm pt-3 pb-9 m-auto text-center">
+      <p className="text-xl text-text_primary mt-5">ความฉลาดรู้ทั่วไป</p>
+      <SubjectInput title="ภาษาไทย" id="thai" />
+      <SubjectInput title="ภาษาอังกฤษ" id="eng" />
+      <SubjectInput title="คณิตศาสตร์" id="math" />
+      <p className="text-xl text-text_primary mt-5">ความฉลาดรู้เฉพาะ</p>
+      <SubjectInput title="วิทยาศาสตร์และเทคโนโลยี" id="sci" />
+      <SubjectInput title="เคมี" id="chem" />
+      <SubjectInput title="ชีววิทยา" id="bio" />
+      <SubjectInput title="ฟิสิกส์" id="phy" />
+    </form>
+  );
+};
 const CapabilityForm = () => {
-  return (<form
-    id="capacity-form"
-    className="text-secondary text-sm pt-3 pb-9 m-auto text-center border-b-2 hidden"
-  >
-    <p className="text-xl text-text_primary mt-5">สมรรถนะเฉพาะด้าน</p>
-    <SubjectInput title="ภาษาต่างประเทศ" id="foreign" />
-    <SubjectInput title="ศิลปกรรมศาสตร์" id="fiart" />
-    <SubjectInput title="วิศวกรรมศาสตร์" id="engi" />
-    <SubjectInput title="ศึกษาศาสตร์" id="edu" />
-    <SubjectInput title="เวชนิทัศน์" id="med" />
-  </form>)
-}
-export {NetsatForm, CapabilityForm}
+  const { selected } = useContext(DataContext);
+  const toggleCapabilityForm = (v: boolean) => {
+    const form = document.getElementById("capability-form");
+    if (form != null) {
+      v ? form.classList.remove("hidden") : form.classList.add("hidden");
+    }
+  };
+  useMemo(() => {
+    let found = false;
+    for (const v of selected) {
+      if (hasCapability(v)) {
+        found = true;
+      }
+    }
+    found ? toggleCapabilityForm(true) : toggleCapabilityForm(false);
+  }, [selected]);
+
+  return (
+    <form
+      id="capability-form"
+      className="text-sm pt-3 pb-9 m-auto text-center md:mt-5"
+    >
+      <p className="text-xl text-text_primary">สมรรถนะเฉพาะด้าน</p>
+      <div className="flex flex-col">
+        <OpenFormButton
+          containerID="languageContainer"
+          title="ภาษาต่างประเทศ"
+          placeholder="ฝรั่งเศส, เยอรมนี, ญี่ปุ่น และเกาหลี"
+        />
+        <OpenFormButton
+          containerID="artContainer"
+          title="ศิลปกรรมศาสตร์"
+          placeholder="การวาดเส้น, องค์ประกอบทางศิลป์, การวาดเส้นเพื่อการสื่อสาร, ออกแบบนิเทศ, ด้านดนตรี และนาฎศิลป์"
+        />
+        <SubjectInput title="วิศวกรรมศาสตร์" id="engi" />
+        <OpenFormButton
+          containerID="eduContainer"
+          title="ศึกษาศาสตร์"
+          placeholder="สมรรถนะทางกาย และความถนัดทางศิลป์"
+        />
+        <OpenFormButton
+          containerID="medContainer"
+          title="เวชนิทัศน์"
+          placeholder="เทคโนโลยีสำหรับเวชนิทัศน์และศิลป์สำหรับเวชนิทัศน์"
+        />
+      </div>
+    </form>
+  );
+};
+export { NetsatForm, CapabilityForm };
