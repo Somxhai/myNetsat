d941d74e13aa71a62de48d07eabf51a0d45064d4
Somxhai <ChanakanThongrong@gmail.com>
2022-09-25 17:03:14 +0700

    remove id

--- src/components/SearchComponents/SearchResults.tsx
+++ src/components/SearchComponents/SearchResults.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useMemo, useState } from "react";
 import SearchList from "../SearchList";
 import { getApi } from "../../features/GetAPI";
-
+import "../../styles/scrollwheel.css";
 const SearchResults = ({ receiveSearch }: any) => {
   const [result, setResult] = useState("");
-  var output: any = useMemo(()=> getApi()
+  const JSONdata = getApi()
+  var output: any = useMemo(()=> JSONdata
       .filter((val) => {
         if (result == "") {
           return val;
@@ -24,12 +25,12 @@ const SearchResults = ({ receiveSearch }: any) => {
   }, [receiveSearch]);
 
   return (
-    <div className="justify-center border-t-2 text-secondary text-center h-72 w-72">
-      <div className="mt-3 overflow-y-auto h-64 md:pr-4">
+    <div className="justify-center border-t-2  text-center h-72 w-72">
+      <div id="searchResult" className="mt-3 overflow-y-auto h-64  first:rounded-t-lg last:rounded-b-lg">
         {output.length > 0 ? (
           output
         ) : (
-          <p>
+          <p className="text-text_secondary">
             แย่จัง หาคณะหรือสาขาไม่เจอ
             <br />
             {"<"}(＿　＿){">"}
