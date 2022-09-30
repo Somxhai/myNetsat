f0aededf6e3e0dfde704c7a14fd75e7fbad4c3c7
Somxhai <ChanakanThongrong@gmail.com>
2022-09-30 20:30:19 +0700

    add footer (from calculation page)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+
+const Footer = () => {
+  
+  return (<div>
+    <div className="text-text_secondary text-center pb-3">
+          Made by{" "}
+          <a
+            target="_blank"
+            title="instagram :-)"
+            href="https://www.instagram.com/peaktoleast/"
+            className="underline text-purple-700 hover:text-purple-500 "
+          >
+            Somxhai
+          </a>
+          <p>
+            ขอบคุณข้อมูลจาก{" "}
+            <a
+              target="_blank"
+              title="โควต้า มข ปี 65"
+              className="underline hover:text-text_primary"
+              href="https://admissions.kku.ac.th/quota65/?fbclid=IwAR1_7d5q1T-Tmfb2wwdLjdasGG7JlgbkOcCYZTb9RBiddJtu1X1UwonXpek"
+            >
+              admissions.kku.ac.th/quota65
+            </a>
+          </p>
+        </div>
+  </div>)
+}
+export default Footer
