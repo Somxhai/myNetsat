587c7656ebb1ab0f7e7725905e70a972ae89ff6b
Somxhai <ChanakanThongrong@gmail.com>
2022-10-01 19:42:04 +0700

    move pattern check to essential feautures

--- src/components/Template/SubjectInput.tsx
+++ src/components/Template/SubjectInput.tsx
@@ -1,6 +1,6 @@
 import React, { useLayoutEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-import { isInputRequired } from "../../features/EssentialFeatures";
+import { isInputRequired, patternCheck } from "../../features/EssentialFeatures";
 import {
   selectedDataState,
 } from "../../States/States";
@@ -16,11 +16,7 @@ const SubjectInput = ({ title, id }: SubjectInputType) => {
     isRequired ? setBgColor("border-purple-600") : setBgColor("border-gray-200")
   }, [selected])
 
-  const patternCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const pattern = "^([0-9]+\\.?[0-9]*|[0-9]*\\.[0-9]+)$";
-    if (value.match(pattern) || value == "") setState(value);
-  };
+  
   return (
     <div className="mt-3">
       <p className="text-text_primary transition-all duration-75">{title}</p>
@@ -29,7 +25,7 @@ const SubjectInput = ({ title, id }: SubjectInputType) => {
         name={id}
         value={state}
         title={title}
-        onChange={(e) => patternCheck(e)}
+        onChange={(e) => setState(patternCheck(e))}
         className={`${bgColor}  bg-white text-sm px-2 text-text_secondary hover:border-purple-500 focus:outline-none rounded-lg border-b-2`}
         maxLength={6}
       />
