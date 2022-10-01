5d0b558cf493c74447b958b5a9cefc31a2efbe7c
Somxhai <ChanakanThongrong@gmail.com>
2022-10-01 19:44:38 +0700

    add min_sum, minimum_score and is_national

--- src/Types/DataType.ts
+++ src/Types/DataType.ts
@@ -1,42 +1,53 @@
 export interface ValType {
-  syllabus: string;
-  faculty: string;
-  syllabus_id: number;
-  weight: Weight;
+  syllabus:                string;
+  faculty:                 string;
+  syllabus_id:             number;
+  has_minimum_score:       boolean;
+  weight:                  Weight;
   has_specific_capability: boolean;
-  specific_capability: SpecificCapability;
+  is_national:             boolean;
+  specific_capability:     SpecificCapability;
+  minimum_score?:          MinimumScore;
+  min_sum?:                number;
 }
-export interface ArgumentValType {
-  key: number | string;
-  value: ValType;
+
+export interface MinimumScore {
+  math?:     number;
+  phy?:      number;
+  eng?:      number;
+  chem?:     number;
+  engineer?: number;
+  thai?:     number;
+  sci?:      number;
+  bio?:      number;
 }
 
 export interface SpecificCapability {
-  foreign: Foreign;
-  fine_arts: FineArts;
-  engineer: number;
-  architect: Architect;
-  education: Education;
+  foreign:        Foreign;
+  fine_arts:      FineArts;
+  engineer:       number;
+  architect:      Architect;
+  education:      Education;
   medical_vision: MedicalVision;
 }
 
 export interface Architect {
-  arch: number;
+  arch:   number;
   design: number;
 }
 
 export interface Education {
-  body: number;
+  body:      number;
   goodatart: number;
 }
 
 export interface FineArts {
   drawing: number;
-  makeup: number;
+  makeup:  number;
   drawcom: number;
-  vart: number;
-  music: number;
-  dance: number;
+  vart:    number;
+  music:   number;
+  dance:   number;
 }
 
 export interface Foreign {
@@ -49,15 +60,14 @@ export interface Foreign {
 
 export interface MedicalVision {
   techmed: number;
-  artmed: number;
+  artmed:  number;
 }
-
 export interface Weight {
   thai: number;
-  eng: number;
+  eng:  number;
   math: number;
-  sci: number;
+  sci:  number;
   chem: number;
-  bio: number;
-  phy: number;
+  bio:  number;
+  phy:  number;
 }
