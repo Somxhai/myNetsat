41a5a399bef9e12270f088f4288c3c0d75d1e343
Somxhai <ChanakanThongrong@gmail.com>
2022-10-03 01:41:45 +0700

    loading page

--- src/page/CalculatePage.tsx
+++ src/page/CalculatePage.tsx
@@ -14,9 +14,13 @@ import {
   calState,
   capabilityID,
   capabilityScore,
+  dataStore,
+  engTestStore,
 } from "../States/States";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import Footer from "../components/Footer";
+import { getApi } from "../features/ApiFeatures";
+import LoadingPage from "./LoadingPage";
 
 const CalculatePage = () => {
   const [blur, setBlur] = useRecoilState(blurScreenState);
@@ -27,7 +31,18 @@ const CalculatePage = () => {
   const engineerFormRef = useRef<HTMLFormElement>(null!);
   const calculatePageRef = useRef<HTMLDivElement>(null!);
   const initial = useRef(true);
+  const [data, setData] = useRecoilState(dataStore);
+  const [engData, setEngData] = useRecoilState(engTestStore);
 
+  useEffect(() => {
+    getApi("netsat").then((res) => {
+      setData(res.data);
+    });
+    getApi("netsat/engtest").then((res) => {
+
+      setEngData(res.data)
+    })
+  }, []);
   useLayoutEffect(() => {
     if (initial.current) {
       initial.current = false;
@@ -37,98 +52,131 @@ const CalculatePage = () => {
       ? calculatePageRef.current.classList.remove("blurScreen")
       : calculatePageRef.current.classList.add("blurScreen");
   }, [blur]);
-  useEffect(() => {
-    setCapScore(() => {
-      const prevClone = structuredClone(capScore);
-      prevClone.fr = parseFloat(capabilityFormRef.current["fr"].value);
-      prevClone.gr = parseFloat(capabilityFormRef.current["gr"].value);
-      prevClone.cn = parseFloat(capabilityFormRef.current["cn"].value);
-      prevClone.jp = parseFloat(capabilityFormRef.current["jp"].value);
-      prevClone.kr = parseFloat(capabilityFormRef.current["kr"].value);
 
-      prevClone.drawing = parseFloat(
-        capabilityFormRef.current["drawing"].value
-      );
-      prevClone.makeup = parseFloat(capabilityFormRef.current["makeup"].value);
-      prevClone.engineer = parseFloat(
-        engineerFormRef.current["engineer"].value
-      );
-      prevClone.drawcom = parseFloat(
-        capabilityFormRef.current["drawcom"].value
-      );
-      prevClone.vart = parseFloat(capabilityFormRef.current["vart"].value);
-      prevClone.music = parseFloat(capabilityFormRef.current["music"].value);
-      prevClone.dance = parseFloat(capabilityFormRef.current["dance"].value);
-
-      prevClone.arch = parseFloat(capabilityFormRef.current["arch"].value);
-      prevClone.design = parseFloat(capabilityFormRef.current["design"].value);
-
-      prevClone.body = parseFloat(capabilityFormRef.current["body"].value);
-      prevClone.goodatart = parseFloat(
-        capabilityFormRef.current["goodatart"].value
-      );
-
-      prevClone.techmed = parseFloat(
-        capabilityFormRef.current["techmed"].value
-      );
-      prevClone.artmed = parseFloat(capabilityFormRef.current["artmed"].value);
-      for (const key of Object.keys(prevClone)) {
-        if (prevClone[key] > 100) prevClone[key] = 100;
-      }
-      return prevClone;
-    });
-  }, [trigger]);
   return (
     <main>
-      <SearchContainer />
-      <form ref={capabilityFormRef} onSubmit={(e) => e.preventDefault()}>
-        <LanguagesContainer />
-        <ArtContainer />
-        <EduContainer />
-        <MedContainer />
-        <ArchContainer />
-      </form>
-      <div
-        id="calculatePage"
-        ref={calculatePageRef}
-        className="w-screen m-auto px-5 overflow-y-hidden"
-      >
-        <div className="my-7 font-mitr relative bg-white px-6 py-3 shadow-sm ring-1 ring-gray-900/5 m-auto max-w-2xl rounded-lg ">
-          <p className="text-text_primary text-2xl text-center">
-            คำนวณคะแนน Netsat
-          </p>
-          <section className="border-b-2 md:flex md:justify-around">
-            <NetsatForm />
-            <form ref={engineerFormRef}>
-              <CapabilityForm />
-            </form>
-          </section>
+      {data.length === 0 && Object.keys(engData).length === 0 ? (
+        <LoadingPage />
+      ) : (
+        <div>
+          <SearchContainer />
+          <form ref={capabilityFormRef} onSubmit={(e) => e.preventDefault()}>
+            <LanguagesContainer />
+            <ArtContainer />
+            <EduContainer />
+            <MedContainer />
+            <ArchContainer />
+          </form>
+          <div
+            id="calculatePage"
+            ref={calculatePageRef}
+            className="w-screen m-auto px-5 overflow-y-hidden"
+          >
+            <div className="my-7 font-mitr relative bg-white px-6 py-3 shadow-sm ring-1 ring-gray-900/5 m-auto max-w-2xl rounded-lg ">
+              <p className="text-text_primary text-2xl text-center">
+                คำนวณคะแนน Netsat
+              </p>
+              <section className="border-b-2 md:flex md:justify-around">
+                <NetsatForm />
+                <form ref={engineerFormRef}>
+                  <CapabilityForm />
+                </form>
+              </section>
 
-          <p className="text-xs text-secondary w-fit mt-3">
-            * ภาษาไทยและภาษาอังกฤษใช้คะแนนร้อยละ
-          </p>
+              <p className="text-xs text-secondary w-fit mt-3">
+                * ภาษาไทยและภาษาอังกฤษใช้คะแนนร้อยละ
+              </p>
 
-          <div className="mt-5 flex font-[Kanit] align-baseline">
-            <button
-              onClick={() => {
-                setBlur(!blur);
-                setForm("searchContainer");
-              }}
-              className="cursor-pointer select-none border-b-2 text-base text-white bg-purple-500 w-fit py-2 px-5 rounded-xl flex m-auto mt-3 hover:bg-purple-300 lg:px-12 lg:mt-5"
-            >
-              เลือกคณะ
-            </button>
-            <button
-              onClick={() => setTrigger(!trigger)}
-              className="cursor-pointer select-none border-b-2 text-base text-white bg-green-500 w-fit py-2 px-5 rounded-xl flex m-auto mt-3 hover:bg-green-300  lg:px-12 lg:mt-5"
-            >
-              คำนวณ
-            </button>
+              <div className="mt-5 flex font-[Kanit] align-baseline">
+                <button
+                  onClick={() => {
+                    setBlur(!blur);
+                    setForm("searchContainer");
+                  }}
+                  className="cursor-pointer select-none border-b-2 text-base text-white bg-purple-500 w-fit py-2 px-5 rounded-xl flex m-auto mt-3 hover:bg-purple-300 lg:px-12 lg:mt-5"
+                >
+                  เลือกคณะ
+                </button>
+                <button
+                  onClick={() => {
+                    setCapScore(() => {
+                      const prevClone = structuredClone(capScore);
+                      prevClone.fr = parseFloat(
+                        capabilityFormRef.current["fr"].value
+                      );
+                      prevClone.gr = parseFloat(
+                        capabilityFormRef.current["gr"].value
+                      );
+                      prevClone.cn = parseFloat(
+                        capabilityFormRef.current["cn"].value
+                      );
+                      prevClone.jp = parseFloat(
+                        capabilityFormRef.current["jp"].value
+                      );
+                      prevClone.kr = parseFloat(
+                        capabilityFormRef.current["kr"].value
+                      );
+
+                      prevClone.drawing = parseFloat(
+                        capabilityFormRef.current["drawing"].value
+                      );
+                      prevClone.makeup = parseFloat(
+                        capabilityFormRef.current["makeup"].value
+                      );
+                      prevClone.engineer = parseFloat(
+                        engineerFormRef.current["engineer"].value
+                      );
+                      prevClone.drawcom = parseFloat(
+                        capabilityFormRef.current["drawcom"].value
+                      );
+                      prevClone.vart = parseFloat(
+                        capabilityFormRef.current["vart"].value
+                      );
+                      prevClone.music = parseFloat(
+                        capabilityFormRef.current["music"].value
+                      );
+                      prevClone.dance = parseFloat(
+                        capabilityFormRef.current["dance"].value
+                      );
+
+                      prevClone.arch = parseFloat(
+                        capabilityFormRef.current["arch"].value
+                      );
+                      prevClone.design = parseFloat(
+                        capabilityFormRef.current["design"].value
+                      );
+
+                      prevClone.body = parseFloat(
+                        capabilityFormRef.current["body"].value
+                      );
+                      prevClone.goodatart = parseFloat(
+                        capabilityFormRef.current["goodatart"].value
+                      );
+
+                      prevClone.techmed = parseFloat(
+                        capabilityFormRef.current["techmed"].value
+                      );
+                      prevClone.artmed = parseFloat(
+                        capabilityFormRef.current["artmed"].value
+                      );
+                      for (const key of Object.keys(prevClone)) {
+                        if (prevClone[key] > 100) prevClone[key] = 100;
+                      }
+                      return prevClone;
+                    });
+                    setTrigger(!trigger);
+                  }}
+                  className="cursor-pointer select-none border-b-2 text-base text-white bg-green-500 w-fit py-2 px-5 rounded-xl flex m-auto mt-3 hover:bg-green-300  lg:px-12 lg:mt-5"
+                >
+                  คำนวณ
+                </button>
+              </div>
+            </div>
+            <CalculateResult />
+            <Footer />
           </div>
         </div>
-        <CalculateResult />
-        <Footer />
-      </div>
+      )}
     </main>
   );
 };
---
+++ src/page/LoadingPage.tsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <main className="select-none flex flex-col h-screen justify-center items-center">
+      <img src="/focus.jpg" className="w-32 h-32 m-auto relative" />
+      <p className="text-text_secondary pb-3 text-center">Made By Somxhai 😈</p>
+    </main>
+  );
+};
+export default LoadingPage;
