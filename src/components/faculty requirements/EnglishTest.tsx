bd71ccff67d0a45c86078cce1d7400b15936b7ee
Somxhai <ChanakanThongrong@gmail.com>
2022-10-03 01:40:44 +0700

    format 1 more time

--- src/components/faculty requirements/EnglishTest.tsx
+++ src/components/faculty requirements/EnglishTest.tsx
@@ -5,36 +5,39 @@ import {
   checkMinEngTestScore,
   isEngineer,
 } from "../../features/EssentialFeatures";
-import { engTestScore, netsatScore } from "../../States/States";
+import { engTestScore, engTestStore, netsatScore } from "../../States/States";
 import { ValTypeArgument } from "../../Types/ArgumentType";
 
 export const EnglishTest = ({ data }: ValTypeArgument) => {
   const engScore = useRecoilValue(engTestScore);
   const netsatInputScore = useRecoilValue(netsatScore);
   const [pass, setPass] = useState(false);
-
+  const engTestData = useRecoilValue(engTestStore);
+  const minEngData = isEngineer(data)
+    ? engTestData["engineer"]
+    : engTestData["pharmarcy"];
+  const minEngScore = getMinEngTestScore(engScore.name, minEngData);
   useEffect(() => {
     setPass(() =>
-      checkMinEngTestScore(engScore.name, engScore.score, data.faculty)
+      checkMinEngTestScore(engScore.name, engScore.score, minEngData)
     );
   }, [netsatInputScore, engScore]);
   return (
-    <>
-      <div className="text-center mt-2">
+    <main className="text-center mt-2">
+      <section>
         <p className="text-lg">คะแนนการทดสอบภาษาอังกฤษ </p>
-
         <p className="text-purple-600 text-center">{engScore.name}</p>
-        <p>
-          {pass ? "คุณผ่านด้วยคะแนน" : "คุณไม่ผ่านด้วยคะแนน"}{" "}
-          <span className={`${pass ? "text-green-500" : "text-red-500"}`}>
-            {engScore.score}
-          </span>
-          /
-          {getMinEngTestScore(
-            engScore.name,
-            isEngineer(data) ? "engineer" : "pharmarcy"
-          )}
-        </p>
+        {isNaN(minEngScore) ? (
+          `หลักสูตรนี้ไม่ใช้ ${engScore.name}`
+        ) : (
+          <p>
+            {pass ? "คุณผ่านด้วยคะแนน" : "คุณไม่ผ่านด้วยคะแนน"}{" "}
+            <span className={`${pass ? "text-green-500" : "text-red-500"}`}>
+              {engScore.score}
+            </span>
+            /{minEngScore}
+          </p>
+        )}
         <p className="text-xs text-text_secondary text-left">
           {engScore.name == "KKU-AELT" &&
             isEngineer(data) &&
@@ -43,7 +46,7 @@ export const EnglishTest = ({ data }: ValTypeArgument) => {
         {!pass &&
           isEngineer(data) &&
           "ต้องได้รับการอบรมเตรียมความพร้อมด้านภาษาอังกฤษ"}
-      </div>
-    </>
+      </section>
+    </main>
   );
 };
