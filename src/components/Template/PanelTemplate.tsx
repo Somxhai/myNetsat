e3661a53a673e4ed97ead38478aab4376b69f951
Somxhai <ChanakanThongrong@gmail.com>
2022-10-06 12:12:22 +0700

    disable scrolling when modal is open

--- src/components/Template/PanelTemplate.tsx
+++ src/components/Template/PanelTemplate.tsx
@@ -1,17 +1,15 @@
 import { useLayoutEffect, useRef } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import { useRecoilState } from "recoil";
-import { blurScreenState, capabilityID } from "../../States/States";
+import { capabilityID } from "../../States/States";
 import { PanelType } from "../../Types/PanelType";
 
 const PanelTemplate = ({ children, id }: PanelType) => {
   const [capabilityId, setCapabilityId] = useRecoilState(capabilityID);
   const elm = useRef<HTMLDivElement>(null!);
-  const [blur, setBlur] = useRecoilState(blurScreenState);
   const closePanel = () => {
     if (elm.current == null) return;
     if (capabilityId == id) {
-      setBlur(!blur);
       setCapabilityId("");
     }
   };
@@ -26,13 +24,11 @@ const PanelTemplate = ({ children, id }: PanelType) => {
       elm.current.classList.add("hidden");
     }
   }, [capabilityId]);
-
   return (
     <div
       ref={elm}
       id={id}
-      // left-1/2 -translate-x-1/2 translate-y-5
-      className="hidden fixed first-line:text-sm md:text-md p-5 max-w-2xl m-auto t w-fit h-fit z-10 bg-white shadow-xl left-1/2 -translate-x-1/2 translate-y-5 rounded-lg"
+      className="hidden fixed md:text-md p-5 z-50 bg-white w-fit shadow-xl rounded-lg left-1/2 -translate-x-1/2 translate-y-1/2"
     >
       {children}
     </div>
